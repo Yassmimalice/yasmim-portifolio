f3bbdb619d23c2c4a53fbf2ef91e4bc18286e125
Yassmimalice <yasmimaliceteixeira16@gmail.com>
2025-01-29 11:47:17 -0300

    Add TestimonialsSection to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { NavBar } from './components/nav-bar'
-import { HeroSection } from './components/hero-section'
-import { AboutSection } from './components/about-section'
-import { SkillsSection } from './components/skills-section'
-import { ProjectsSection } from './components/projects-section'
-import { ContactSection } from './components/contact-section'
-import { TechStack } from './components/tech-stack'
-import { CustomCursor } from './components/custom-cursor'
+import { NavBar } from "./components/nav-bar"
+import { HeroSection } from "./components/hero-section"
+import { AboutSection } from "./components/about-section"
+import { SkillsSection } from "./components/skills-section"
+import { ProjectsSection } from "./components/projects-section"
+import { TestimonialsSection } from "./components/testimonials"
+import { ContactSection } from "./components/contact-section"
+import { TechStack } from "./components/tech-stack"
+import { CustomCursor } from "./components/custom-cursor"
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <SkillsSection />
       <TechStack />
       <ProjectsSection />
+      <TestimonialsSection />
       <ContactSection />
     </main>
   )
